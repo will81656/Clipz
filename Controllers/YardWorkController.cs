82685173b91bea540314d5ca679b03968f3db84f
William casteel <will81656@gmail.com>
2021-04-07 16:58:25 -0500

    personalized

---
+++ Controllers/YardWorkController.cs
@@ -0,0 +1,23 @@
+﻿using Clipz.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clipz.Controllers
+{
+    [Authorize]
+    public class YardWorkController : Controller
+    {
+        public ActionResult Index()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new YardWorkService(userId);
+            var model = service.GetYardWorkList();
+
+            return View(model);
+        }
+    }
+}
